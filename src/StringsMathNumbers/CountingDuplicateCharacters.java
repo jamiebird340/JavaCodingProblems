13f871bab101b5a0feaa6da8fbbeff465f7a4b41
Jamie Bird <jamieb@brokerinsights.com>
2022-05-25 21:12:08 +0100

    Initial commit to create project

---
+++ src/StringsMathNumbers/CountingDuplicateCharacters.java
@@ -0,0 +1,54 @@
+package StringsMathNumbers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CountingDuplicateCharacters {
+
+    static final Logger LOGGER = Logger.getLogger("");
+
+    private static final String EXAMPLE_TEXT = "example_text_1234";
+
+    public static void main(String[] args) {
+        countCharactersUsingMap();
+        printDuplicateCharacterCount();
+    }
+
+    public static void countCharactersUsingMap() {
+        LOGGER.log(Level.INFO, ()-> String.valueOf(characterMap(EXAMPLE_TEXT)));
+    }
+
+    public static Map<Character, Integer> characterMap(String str) {
+        Map<Character, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+
+        return result;
+    }
+
+    public static void printDuplicateCharacterCount(){
+        LOGGER.log(Level.INFO, ()-> String.valueOf(countDuplicateCharacters(EXAMPLE_TEXT)));
+    }
+
+    public static Map<String, Integer> countDuplicateCharacters(String str){
+        Map<String, Integer> result = new HashMap<>();
+
+        for (int i = 0; i < str.length(); i++){
+            int cp = str.codePointAt(i);
+            String ch = String.valueOf(Character.toChars(cp));
+            if(Character.charCount(cp) == 2){
+                i++;
+            }
+
+            result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+
+        return result;
+    }
+
+}
---
+++ src/StringsMathNumbers/FindingFirstNonRepeatedCharacter.java
@@ -0,0 +1,35 @@
+package StringsMathNumbers;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class FindingFirstNonRepeatedCharacter {
+
+    static final Logger LOGGER = Logger.getLogger("");
+
+    private static final String EXAMPLE_TEXT = "example_text_1234";
+
+    public static void printFirstNonRepeatingCharacter(){
+        LOGGER.log(Level.INFO, ()-> String.valueOf(firstNonRepeatedCharacter(EXAMPLE_TEXT)));
+    }
+
+    public static char firstNonRepeatedCharacter(String str) {
+        Map<Character, Integer> chars = new LinkedHashMap<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            chars.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+
+        for (Map.Entry<Character, Integer> entry: chars.entrySet()){
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return Character.MIN_VALUE;
+    }
+}
